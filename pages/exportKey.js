b69b8a600661147b7c1f672ff6c3a116ea6983f9
Candid Dauth <cdauth@cdauth.eu>
2013-01-16 03:57:10 +0100

    Fixing key export

--- pages/exportKey.js
+++ pages/exportKey.js
@@ -1,16 +1,11 @@
-/*var utils = require("../utils");
-var keyrings = require("../keyrings");
+var utils = require("../utils");
 var keys = require("../keys");
 
 exports.get = function(req, res, next) {
 	var formatInfo = utils.getInfoForFormat(req.query.exportFormat);
 	res.attachment("0x"+req.params.keyId+formatInfo.extension);
 	res.type(formatInfo.mimetype);
-	
-	var keyring = null;
-	if(req.session.user)
-		keyring = keyrings.getKeyringForUser(req.session.user.id);
-	
+
 	var selection = null;
 	
 	if(req.query.selection)
@@ -37,7 +32,7 @@ exports.get = function(req, res, next) {
 		}
 	}
 	
-	utils.encodeToFormat(keys.exportKey(req.params.keyId, keyring, selection), req.query.exportFormat).whilst(function(data, cb) {
+	utils.encodeToFormat(req.keyring.exportKey(req.params.keyId, selection), req.query.exportFormat).whilst(function(data, cb) {
 		res.write(data, "binary");
 		cb();
 	}, function(err) {
@@ -46,4 +41,4 @@ exports.get = function(req, res, next) {
 		else
 			res.end();
 	});
-}*/
+};
