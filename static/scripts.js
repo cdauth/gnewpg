de6775bde64de9e190a659fa56c6256a3e69c3b0
Candid Dauth <cdauth@cdauth.eu>
2013-02-25 13:09:31 +0100

    Adding filter input for keyring

--- static/scripts.js
+++ static/scripts.js
@@ -9,6 +9,33 @@ $(document).ready(function() {
 		tr.toggleClass("success");
 		$("input[type=checkbox]", tr).prop("checked", tr.hasClass("success"));
 	});
+
+	$(".table-with-filter").each(function() {
+		var t = $(this);
+
+		var form = $('<form class="form-search"><input type="text" class="search-query" autocomplete="off" /></form>').insertBefore(t);
+		$("input", form).attr("placeholder", i18n["Filter"]).bind("keyup click change", function() {
+			var val = $(this).val().trim().toLowerCase();
+			var rows = $("> tbody > tr,> tr", t);
+			if(val == "")
+				rows.css("display", "");
+			else
+			{
+				rows.each(function() {
+					var contains = false;
+					$("> td", this).each(function() {
+						if($(this).text().toLowerCase().indexOf(val) != -1)
+						{
+							contains = true;
+							return false;
+						}
+					});
+					$(this).css("display", contains ? "" : "none");
+					$("input", this).prop("disabled", !contains);
+				});
+			}
+		});
+	});
 });
 
 function quoteRegexp(str) {
