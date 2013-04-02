5007b08de3f5228c3bd2e8f05daa675c52e9155e
Candid Dauth <cdauth@cdauth.eu>
2013-04-02 16:13:31 +0200

    Only showing keyserver URL if keyserver is enabled

--- web/index.js
+++ web/index.js
@@ -1,10 +1,12 @@
 var config = require("../config");
 
 exports.get = function(req, res, next) {
-	if(req.session.user)
-		req.params.keyserver = "hkps://"+config.personalHkpHostname.replace("%s", req.session.user.secret);
-	else
-		req.params.keyserver = "hkps://"+config.hkpHostname;
+	if(config.hkpHostname) {
+		if(req.session.user)
+			req.params.keyserver = "hkps://"+config.personalHkpHostname.replace("%s", req.session.user.secret);
+		else
+			req.params.keyserver = "hkps://"+config.hkpHostname;
+	}
 
 	next();
 };
